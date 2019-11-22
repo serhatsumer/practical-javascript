48dad3f4d40420b05b3c8a4662f38c1eed923d0d
serhat <serhat-sumer@hotmail.com>
2019-11-22 18:05:58 +0100

    Version-2 Functions added

--- todo.js
+++ todo.js
@@ -3,8 +3,8 @@
 // It should have a place to todos
 // It should have a way to display todos
 // It should have a way to add new todos
-// It should have a way to change a todo
-// It should have a way to delete todo
+// It should have a way to change a todos
+// It should have a way to delete todos
 
 // It should have a place to todo
 var todos = ['item1','item2','item3']
@@ -20,3 +20,37 @@ todos[index] = 'new item';
 
 // It should have a way to delete todo
 todos.splice(index, Num);
+
+
+// Version-2 Functions
+// Requirements
+// It should have a function to display todos
+// It should have a function to add todos
+// It should have a function to change a todos
+// It should have a function to delete todos
+
+// It should have a function to display todos
+function displayTodos(){
+    console.log('My todos: ',todos); // expected --> My todos: ['item1', 'item2', 'item3']
+}
+// It should have a function to add todos
+function addTodo(param) {
+    todos.push(param);
+    displayTodos();   // expected --> My todos: ['item1', 'item2', 'item3','param']
+}
+// It should have a function to change a todos
+function changeTodo(index, param){
+    todos[index] = param;
+    displayTodos();
+}
+
+// changeTodo(1, todo); expected --> My todos: ['item1', 'todo', 'item3','param']
+
+// It should have a function to delete todos
+function deleteTodo(index){
+    todos.splice(index, 1);
+    displayTodos();
+}
+
+// deleteTodo(3); expected --> My todos: ['item1', 'todo', 'item3']
+
