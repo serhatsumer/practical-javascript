3c4a887303654775dabe17f778db9f327d697937
serhat <serhat-sumer@hotmail.com>
2019-11-28 20:54:57 +0100

    refactor: replace for loops with forEach

--- todo.js
+++ todo.js
@@ -1,10 +1,7 @@
-// Version 10 - Click to delete
+// Version 11 - Destroy all for loops
 // Requirements
-// There should be a way to create delete buttons
+// todoList.toggleAll should use forEach
 // There should be a delete button for each todo
-// Each li should have an id that has the todo position
-// Delete buttons should have access to the todo id 
-// Clicking delete should update todoList.todos and the DOM
 
 var todoList = {
     todos: [],
@@ -25,23 +22,20 @@ var todoList = {
         todo.completed = !todo.completed;
     },
     toggleAll: function (){
-        let totalTodos = this.todos.length;
-        let completedTodos = 0;
-        for (let i=0 ; i<totalTodos ; i++){
-            if (this.todos[i].completed === true){
-            completedTodos++;
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        this.todos.forEach(function(todo){
+            if ( todo.completed === true){
+                completedTodos++;
             }
-        } 
+        });  
+        this.todos.forEach(function(todo){      // if everything is true, make everything false
             if (completedTodos === totalTodos){
-                for (let i=0 ; i<totalTodos ; i++){
-                this.todos[i].completed = false;
-                }                                   // if everything is true, make everything false
-
-            } else {
-                for (let i=0 ; i<totalTodos ; i++){
-              this.todos[i].completed = true;  
-                }                                   // Otherwise, make everything true
-            }
+                todo.completed = false;
+            } else {                           // Otherwise, make everything true 
+                todo.completed = true;
+              }
+        });
     }     
 };
 
@@ -77,25 +71,25 @@ var handlers = {
         todoList.toggleAll();
         view.displayTodos();
     }
-  };
+};
 var view = {
     displayTodos: function() {
         var todosUl = document.querySelector("ul");
         todosUl.innerHTML = "";
-        for (var i = 0; i < todoList.todos.length; i++) {
+        
+        todoList.todos.forEach(function(todo, position){
             var todoLi = document.createElement("li");
-            var todo = todoList.todos[i];
             var todoTextWithCompletion = "";
             if (todo.completed === true) {
                 todoTextWithCompletion = "(x)" + todo.todoText;
             } else {
                 todoTextWithCompletion = "( )" + todo.todoText;
-            }
-            todoLi.id = i;
+              }
+            todoLi.id = position;
             todoLi.textContent = todoTextWithCompletion;
             todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
-        }
+        }, this) // this refers to view object ==> forEach(callbak, this)
     },
     createDeleteButton: function (){
         var deleteButton = document.createElement('button');
